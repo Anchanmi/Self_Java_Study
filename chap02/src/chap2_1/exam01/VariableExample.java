0d96b5af2a84722f7585304d7962dea2739be33c
Chanmi An <bangoul345@gmail.com>
2023-09-18 19:35:49 +0900

    변수

---
+++ chap02/src/chap2_1/exam01/VariableExample.java
@@ -0,0 +1,14 @@
+package chap2_1.exam01;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 50;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+
+}

4ff1a4a0cf4bf20cb3d3d1c878323c35d65c7cc2
Chanmi An <bangoul345@gmail.com>
2023-09-18 19:37:08 +0900

    기본 타입(정수, 실수, 논리)

---
+++ chap02/src/chap2_2/exam04/CharExample.java
@@ -0,0 +1,23 @@
+package chap2_2.exam04;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c1 = 'A';
+		char c2 = 65;
+		char c3 = '\u0041';
+		
+		char c4 = '°¡';
+		char c5 = 44032;
+		char c6 = '\uac00';
+		
+		System.out.println("c1 : " + c1);
+		System.out.println("c2 : " + c2);
+		System.out.println("c3 : " + c3);
+		System.out.println("c4 : " + c4);
+		System.out.println("c5 : " + c5);
+		System.out.println("c6 : " + c6);
+	}
+
+}

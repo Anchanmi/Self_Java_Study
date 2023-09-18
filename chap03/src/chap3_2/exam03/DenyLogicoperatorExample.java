ee82447b85cd1bd8aa0b1a499aee18d65d4423af
Chanmi An <bangoul345@gmail.com>
2023-09-18 19:40:02 +0900

    연산자의 종류 예제

---
+++ chap03/src/chap3_2/exam03/DenyLogicoperatorExample.java
@@ -0,0 +1,17 @@
+package chap3_2.exam03;
+
+public class DenyLogicoperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+	}
+
+}

6d44c293a29cd6dd0b3063c650cef430e84d46d9
Chanmi An <bangoul345@gmail.com>
2023-09-19 14:48:04 +0900

    스레드 예제(멀티 스레드, 스레드 제어)

---
+++ chap12/src/chap12_1/exam04/BeepPrintExample4.java
@@ -0,0 +1,16 @@
+package chap12_1.exam04;
+
+public class BeepPrintExample4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread thread = new BeepThread();
+		thread.start();
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("¶ò");
+			try {Thread.sleep(500);} catch(Exception e) {}
+		}
+	}
+
+}

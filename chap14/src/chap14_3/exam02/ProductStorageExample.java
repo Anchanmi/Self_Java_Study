306bc091ae3d28383005e7cb8bc5f26300d0252d
Chanmi An <bangoul345@gmail.com>
2023-09-19 14:49:15 +0900

    입출력 스트림 예제(입출력 스트림, 보조 스트림, 입출력 관련 API)

---
+++ chap14/src/chap14_3/exam02/ProductStorageExample.java
@@ -0,0 +1,11 @@
+package chap14_3.exam02;
+
+public class ProductStorageExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ProductStorage productStroage = new ProductStorage();
+		productStroage.showMenu();
+	}
+
+}
